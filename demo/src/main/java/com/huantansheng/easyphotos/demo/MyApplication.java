fb12de4908673d04253c084ddbb03b0331a588ce
honghengqiang <honghengqiang@gmail.com>
2023-05-09 15:03:55 +0800

    d

---
+++ demo/src/main/java/com/huantansheng/easyphotos/demo/MyApplication.java
@@ -0,0 +1,15 @@
+package com.huantansheng.easyphotos.demo;
+
+import android.app.Application;
+
+/**
+ * 主要用于检测内存泄漏
+ * Created by huan on 2018/1/30.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
