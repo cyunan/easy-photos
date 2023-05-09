2846daa2ee0e61346ac2fea12d1adc1bb68d1267
honghengqiang <honghengqiang@gmail.com>
2023-05-09 16:22:22 +0800

    d

--- easyPhotos/src/main/java/com/huantansheng/easyphotos/ui/EasyPhotosActivity.java
+++ easyPhotos/src/main/java/com/huantansheng/easyphotos/ui/EasyPhotosActivity.java
@@ -212,6 +212,9 @@ public class EasyPhotosActivity extends AppCompatActivity implements AlbumItemsA
 
     protected String[] getNeedPermissions() {
         if (Setting.isShowCamera) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                return new String[]{Manifest.permission.CAMERA,Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_AUDIO, Manifest.permission.READ_MEDIA_VIDEO};
+            }
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                 return new String[]{Manifest.permission.CAMERA,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
